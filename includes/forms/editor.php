ca3fb925f72de35a1af75e76b71d2af1161d9922
advalgo <78273609+advalgo@users.noreply.github.com>
2021-12-04 10:09:41 -0700

    Increased size of width/height element

--- includes/forms/editor.php
+++ includes/forms/editor.php
@@ -2,8 +2,8 @@
     <div class="editor_header_wrapper">
         <h1 class="editor_header">
             <span class="editor_sizing">Editor Height <input class="editor_height_text"
-                id="editor_height_text" title="Editor Height" size="1" name="editor_height" type="text">Width <input class="editor_width_text"
-                id="editor_width_text" title="Editor Width" size="1" name="editor_width" type="text">
+                id="editor_height_text" title="Editor Height" size="4" name="editor_height" type="text">Width <input class="editor_width_text"
+                id="editor_width_text" title="Editor Width" size="4" name="editor_width" type="text">
             <div class="working_on" id="working_on"></div></span><div class="shrink_editor" title="Minimize Editor">Shrink</div>
             <div class="expand_editor" title="Maximize Editor">Expand</div>
         </h1>
